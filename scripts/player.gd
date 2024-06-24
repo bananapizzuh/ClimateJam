61233897f70402d2e7969fcb51612dddd7f64500
OJSIMPSION <josef7piper@gmail.com>
2024-06-24 15:40:44 -0400

    ADDED JUMP BUFF

--- scripts/player.gd
+++ scripts/player.gd
@@ -14,23 +14,56 @@ var paused = false
 @onready var collision_shape = $CollisionShape2D
 @onready var wall_jump_timer = $WallJumpTimer
 
+@export var Jump_Buffer_Time: float  = 0.2
+
+var jump_buffer:bool = false
+var able_to_jump:bool = false 
+
 func _physics_process(delta):
 	if paused:
 		return
 
+
+
+
+
+	# jump stuuf
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
 	var direction = Input.get_axis("move_left", "move_right")
 
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump"):
+		if able_to_jump:
+			velocity.y = JUMP_VELOCITY
+			jump_buffer = false
+		else:
+			jump_buffer = true
+			get_tree().create_timer(Jump_Buffer_Time).timeout.connect(on_jump_buffer)
+	if able_to_jump and jump_buffer:
 		velocity.y = JUMP_VELOCITY
+		jump_buffer = false
+	
+	if is_on_floor():
+		able_to_jump = true
+	else: 
+		able_to_jump = false
 
+
+
+
+
+	# direction change
 	if direction > 0:
 		animated_sprite.flip_h = false
 	elif direction < 0:
 		animated_sprite.flip_h = true
 
+
+
+
+
+	# wall jump stuff
 	var wall_direction = 1 if animated_sprite.flip_h else -1
 
 	if is_on_wall():
@@ -43,6 +76,11 @@ func _physics_process(delta):
 		elif velocity.y > 0:
 			velocity.y = lerp(velocity.y, 0.0, FRICTION)
 
+
+
+
+
+	#idle stuff
 	if is_on_floor():
 		if direction == 0:
 			animated_sprite.play("idle")
@@ -51,6 +89,11 @@ func _physics_process(delta):
 	else:
 		animated_sprite.play("jump")
 
+
+
+
+
+	# more jump stuff I GESS
 	if direction:
 		if (is_on_wall() and wall_jump_timer.time_left > 0.0) or wall_direction == direction:
 			pass
@@ -61,4 +104,15 @@ func _physics_process(delta):
 	elif not is_on_wall() and wall_jump_timer.time_left <= 0.0:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
+
+
+
+
 	move_and_slide()
+
+
+
+
+
+func on_jump_buffer():
+		jump_buffer = false
