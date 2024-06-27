148bbcd283e88751601378c3b9efe7e7ed3a6b39
OJSIMPSION <josef7piper@gmail.com>
2024-06-26 21:26:45 -0400

    Extreamly Basic Save File - needs more work

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,5 +1,11 @@
 extends Control
 
+func _ready():
+	if not get_saved_game():
+		save_game(1)
+		print("not")
+	print(get_saved_game())
+
 
 func _on_button_pressed():
 	SceneManager.change_scene(
@@ -8,12 +14,23 @@ func _on_button_pressed():
 		SceneManager.create_options(0.5),
 		SceneManager.create_general_options()
 	)
+	save_game(2)
 
 
 func _on_button_2_pressed():
-	SceneManager.change_scene(
-		"level_2",
-		SceneManager.create_options(0.5),
-		SceneManager.create_options(0.5),
-		SceneManager.create_general_options()
-	)
+	if get_saved_game() >= 2:
+		SceneManager.change_scene(
+			"level_2",
+			SceneManager.create_options(0.5),
+			SceneManager.create_options(0.5),
+			SceneManager.create_general_options()
+		)
+
+func save_game(leval_unlocked : int):
+	var new_file = FileAccess.open("user://savegame.save", FileAccess.WRITE)
+	print(str(leval_unlocked))
+	new_file.store_line(str(leval_unlocked))
+
+func get_saved_game():
+	var file = FileAccess.open("user://savegame.save", FileAccess.READ)
+	return int(file.get_as_text())
