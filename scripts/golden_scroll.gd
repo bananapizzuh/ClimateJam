54c21f3907aede9c14ca0554e1e64a4746b49c01
OJSIMPSION <josef7piper@gmail.com>
2024-07-11 20:22:41 -0400

    added saving to golden golden_scroll

--- scripts/golden_scroll.gd
+++ scripts/golden_scroll.gd
@@ -2,12 +2,20 @@ extends Area2D
 
 @export var timeline_path: String
 
-var collected: bool = false
+var collected: bool = SaveManager.get_saved_scroll()
 
 @onready var mayor_blum: Area2D = %MayorBlum
+
+func _ready():
+	if collected:
+		mayor_blum.load_timeline("res://dialogic/styles/Default_Style.tres", timeline_path)
+		mayor_blum.interacted_with = false
+		queue_free()
+
 
 func _on_body_entered(_body):
 	if not collected:
 		mayor_blum.load_timeline("res://dialogic/styles/Default_Style.tres", timeline_path)
 		mayor_blum.interacted_with = false
+		SaveManager.save_scroll(true)
 		queue_free()
--- scripts/save_manager.gd
+++ scripts/save_manager.gd
@@ -11,22 +11,22 @@ extends Node
 var save = {
 	"level_unlocked": 1,
 	"time": 0,
+	"golden_scroll" : false
 }
-
 # ==============================================================================
 
 func read_save() -> Dictionary:
 # ==============================================================================
 # Read the save file and return the data
 
-    # Check if the save file exists, if not create a new one
+	# Check if the save file exists, if not create a new one
 	if not FileAccess.file_exists("user://savegame.save"):
 		var new_file = FileAccess.open("user://savegame.save", FileAccess.WRITE)
 		new_file.store_string(JSON.stringify(save))
 
 	var file = FileAccess.open("user://savegame.save", FileAccess.READ)
 	var data = file.get_as_text()
-    # Parse the data from the save file
+	# Parse the data from the save file
 	var recieved = JSON.parse_string(data)
 
 # ==============================================================================
@@ -34,13 +34,28 @@ func read_save() -> Dictionary:
 # ==============================================================================
 # Check if the data is valid
 
-    # If the data is invalid, print an error message and return the default save data
+	# If the data is invalid, print an error message and return the default save data
 	if recieved == null or typeof(recieved) != TYPE_DICTIONARY:
 		print("Error: Could not parse save file")
 		return save
 
 	return recieved
 # ==============================================================================
+
+func save_scroll(is_collected: bool) -> void:
+#===============================================================================
+# Save the sate of the golden scroll
+	var file = FileAccess.open("user://savegame.save", FileAccess.WRITE)
+	save["golden_scroll"] = is_collected
+	file.store_string(JSON.stringify(save))
+#===============================================================================
+
+func get_saved_scroll() -> bool:
+#===============================================================================
+# Get the state of the golden scroll
+	var data = read_save()
+	return data["golden_scroll"]
+#===============================================================================
 
 func save_time(time_to_save: float) -> void:
 # ==============================================================================
